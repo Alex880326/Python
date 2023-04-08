81dc4e21ae4e33e4b8e16f96d692e918c8b545d4
Alex880326 <124200830+Alex880326@users.noreply.github.com>
2023-04-08 17:50:36 +0300

    Delete from random import randint.py

--- 1/from random import randint.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-n=int(input('Введите количество человек: '))
-a=[randint(140, 220) for i in range(n)]
-print(a)
-summ=0
-for i in range(n):
- summ+=a[i]
-print('Средний рост =',summ/n)
